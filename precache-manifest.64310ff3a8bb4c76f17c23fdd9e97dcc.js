fdcdca5a8b591f8ccba9503cf19ff6c383d38853
Irena Gao <ijg24@cornell.edu>
2020-01-04 01:07:18 -0500

    Updates

---
+++ precache-manifest.64310ff3a8bb4c76f17c23fdd9e97dcc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb8576fcb54784da264a92dcceaf470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90405d379a13da2930b0",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6e577a417760ad3944b1",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "90405d379a13da2930b0",
+    "url": "/static/js/2.49b5cc29.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.49b5cc29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e577a417760ad3944b1",
+    "url": "/static/js/main.4d575e59.chunk.js"
+  },
+  {
+    "revision": "6f5f2b737ce10e932bbd",
+    "url": "/static/js/runtime-main.7e34de1d.js"
+  },
+  {
+    "revision": "06137aae4e8ddcb9e25644e69aec26d9",
+    "url": "/static/media/Log-in Page.06137aae.jpg"
+  },
+  {
+    "revision": "0912c1f17b95289f0ffd4b9882af284d",
+    "url": "/static/media/Sign-up Page.0912c1f1.jpg"
+  },
+  {
+    "revision": "61d2f9ce011641cdd13f3a8279bd5ea0",
+    "url": "/static/media/amazonscreenshot.61d2f9ce.jpg"
+  },
+  {
+    "revision": "c6cb1f38b5ceabaf6357607fdda28a10",
+    "url": "/static/media/dwblogo.c6cb1f38.png"
+  },
+  {
+    "revision": "97d03df27e434b06a2dd681cfb5abcfe",
+    "url": "/static/media/dynalec.97d03df2.png"
+  },
+  {
+    "revision": "191022b1595a6003a2b54ab76ae9b39e",
+    "url": "/static/media/finalban1.191022b1.png"
+  },
+  {
+    "revision": "a3532d456d08300db59f6e675c7c3f1a",
+    "url": "/static/media/finalbanner.a3532d45.png"
+  },
+  {
+    "revision": "7800d1ffcf78b68844ad8d18bd94064b",
+    "url": "/static/media/home.7800d1ff.png"
+  },
+  {
+    "revision": "385e8310edaf8de893b6bb6356502b12",
+    "url": "/static/media/initialdesignbanner.385e8310.png"
+  },
+  {
+    "revision": "5614ad6aa80a190993d3de80358e5b83",
+    "url": "/static/media/irenaresize.5614ad6a.jpg"
+  },
+  {
+    "revision": "f4cbf2695007fb48feba9f09006c0d57",
+    "url": "/static/media/kickstarter.f4cbf269.png"
+  },
+  {
+    "revision": "aa632f3adc32442adbc363732a13a658",
+    "url": "/static/media/kickstartersite.aa632f3a.png"
+  },
+  {
+    "revision": "f513b855b3f1da2ce93bc3641b66db08",
+    "url": "/static/media/lowfiamazon.f513b855.png"
+  },
+  {
+    "revision": "61c654c55c8e47a0d08bb9547da8344e",
+    "url": "/static/media/mainresume.61c654c5.pdf"
+  },
+  {
+    "revision": "33e864e2c39ab33566c7e19c63532674",
+    "url": "/static/media/marketitem.33e864e2.png"
+  },
+  {
+    "revision": "cbca4030e7ce0cf09eaf34edcdc67035",
+    "url": "/static/media/marketplace.cbca4030.png"
+  },
+  {
+    "revision": "32dcc340e40c2f09eb8ff27fae594b77",
+    "url": "/static/media/microdotlogo.32dcc340.png"
+  },
+  {
+    "revision": "26bd6c7dae6a64b3669da971118f9ac1",
+    "url": "/static/media/microdotprofilebanner.26bd6c7d.png"
+  },
+  {
+    "revision": "d2456a9905c7c7b9ff7850da52a0ca50",
+    "url": "/static/media/snapchat.d2456a99.png"
+  },
+  {
+    "revision": "5b16da9bdf60f896be4e7a30d5f14942",
+    "url": "/static/media/userflowmicrodot.5b16da9b.jpg"
+  },
+  {
+    "revision": "e827eccd4c30dff6e1ea356404d72c27",
+    "url": "/static/media/vemo.e827eccd.png"
+  },
+  {
+    "revision": "69d102d7ffcedefd17aef7fcf89339a4",
+    "url": "/static/media/walletbanner.69d102d7.png"
+  }
+]);
