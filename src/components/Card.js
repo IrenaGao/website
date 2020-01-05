c00beaea0c9418ff9ac84aee71ccd6cc313f202c
Chen <andrewchen349@gmail.com>
2020-01-04 21:51:28 -0500

    Fix Card Padding

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,10 +12,10 @@ import Microdot from "../pages/Microdot";
 
 function Card(props) {
     return(
-        <div className="d-inline-block g-card mt-5 ml-5">
+        <div className="d-inline g-card mt-5">
             <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
             <div className="card border-light mb-3">
-                <div class="card-body shadow-sm p-3 mb-5 bg-white rounded">
+                <div class="card-body shadow-sm mb-5 bg-white rounded">
                     <h3 class="card-title display-5 font-weight-semibold">{props.item.title}</h3>
                     <h5 class="card-text display-6 font-weight-light">{props.item.subTitle}</h5>
                     <Link className="card-link" to={props.item.link}>Learn More</Link>
